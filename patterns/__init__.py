99ff574ff8a0f1678645492bce01915a5797df7f
pen-dev <pelipenkovladislav@gmail.com>
2021-01-23 18:19:19 +0300

    коммит был сделан по ошибке

--- patterns/__init__.py
+++
@@ -1,9 +0,0 @@
-from pytest import (fixture, mark, raises, main, yield_fixture, PytestWarning,
-                    PytestUnknownMarkWarning, PytestCollectionWarning)
-
-
-def test(test: int, number: int, d):
-    pass
-
-
-test(1, 2, 3)
